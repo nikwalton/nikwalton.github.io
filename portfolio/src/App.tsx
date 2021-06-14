b43bf32b7fc45bdb806f15c5ca86dde424b51d16
Nikolaus Walton <nikawalton2@gmail.com>
2021-06-13 21:30:35 -0700

    Project setup and created navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      title: "Nikolaus Walton",
+      header: [
+        {title: 'Home', path: '/'},
+        {title: 'About', path: '/about'},
+        {title: 'Projects', path: '/projects'},
+        {title: 'Art', path:'/art'},
+        {title: 'Contact', path:'/contact'}
+      ],
+      home: {
+        title: 'Technology and Art',
+        subtitle: 'Making a difference and telling stories using technology'
+      },
+      about: {
+        title: 'About'
+      },
+      projects: {
+        title: 'Projects'
+      },
+      art: {
+        title: 'Art'
+      },
+      contact: {
+        title: 'Find Me',
+        subtitle: 'Let\'s have a chat'
+      }
+    }
+  }
+
+  render()
+  {
+    return (
+      <Router>
+        
+        <Container className='p-1' fluid={true}>
+          <Navbar className='border-bottom' expand='lg' bg='transparent'>
+            <Navbar.Brand>Nik Walton</Navbar.Brand>
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle'/> 
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ms-auto'>
+                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/'>About</Link>
+                <Link className='nav-link' to='/'>Projects</Link>
+                <Link className='nav-link' to='/'>Art</Link>
+                <Link className='nav-link' to='/'>Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
