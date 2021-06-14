674a1ab359af8f3bfbab5e04cb529ac02219f2de
Nikolaus Walton <nikawalton2@gmail.com>
2021-06-13 22:24:59 -0700

    Added simple footer component

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -7,6 +7,8 @@ import Nav from 'react-bootstrap/Nav'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Footer from './Components/Footer'
+
 class App extends React.Component {
 
   constructor(props: any) {
@@ -46,20 +48,22 @@ class App extends React.Component {
       <Router>
         
         <Container className='p-1' fluid={true}>
-          <Navbar className='border-bottom' expand='lg' bg='transparent'>
+          <Navbar expand='lg' bg='transparent'>
             <Navbar.Brand>Nikolaus Walton</Navbar.Brand>
             <Navbar.Toggle className='border-0' aria-controls='navbar-toggle'/> 
             <Navbar.Collapse id='navbar-toggle'>
               <Nav className='ms-auto'>
                 <Link className='nav-link' to='/'>Home</Link>
-                <Link className='nav-link' to='/about'>About</Link>
-                <Link className='nav-link' to='/projects'>Projects</Link>
-                <Link className='nav-link' to='/art'>Art</Link>
-                <Link className='nav-link' to='/contact'>Contact</Link>
+                <Link className='nav-link' to='#about'>About</Link>
+                <Link className='nav-link' to='#projects'>Projects</Link>
+                <Link className='nav-link' to='#art'>Art</Link>
+                <Link className='nav-link' to='#contact'>Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
         </Container>
+
+        <Footer />
       </Router>
     );
   }
---
+++ portfolio/src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Footer = () => (
+    <div className='footer'>
+        <small> nikolaus walton </small>
+    </div>
+);
+
+export default Footer;
