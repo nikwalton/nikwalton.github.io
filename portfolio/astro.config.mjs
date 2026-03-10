70c5b909ce5e50939737ffcfe9e5a0e5844616c1
Nikolaus Walton <nikawalton2@gmail.com>
2026-03-10 14:30:47 -0700

    revert astro config, figuring out github workflows

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikwalton.github.io",
-  base: "/website",
+  base: "/portfolio",
   integrations: [react()],
 
   vite: {
